d34ece7c17d870237b9cb578833099247bbfd74c
Muhammad bin Tariq <100954945+bintariq50@users.noreply.github.com>
2025-08-16 10:30:44 +0500

    Update Search.jsx

--- src/Search.jsx
+++ src/Search.jsx
@@ -22,7 +22,7 @@ export default function Search({ updateInfo }) {
 
     const getCityWeatherInfo = async () => {
         const API_URL = "https://api.openweathermap.org/data/2.5/weather"
-        const API_KEY = "f135df4e81376db8b28d9bdc47524cde"
+        const API_KEY = ""
         let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
         let json = await response.json()
         let result = {
